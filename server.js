aed38db76e9abc7bcde2112978352e7f00a3cdbb
Abdelhadi <abdo.sabani30@gmail.com>
2021-04-26 21:50:38 +0000

    Add initial http server

---
+++ server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const app = require("./app");
+const http = require("http");
+
+// create instance from http
+const server = http.createServer(app);
+
+const PORT = app.get("port");
+const ENV = app.get("env");
+const DEBUGGER = app.get("debugger");
+
+// start listening on port specified
+server.listen(PORT);
+
+server.on("listening", () => {
+    console.log(`* Environment : ${ENV}`);
+    console.log(`* Debugger : ${DEBUGGER ? "On" : "Off"}`);
+    console.log(`* Running on http://localhost:${PORT} (CTRL + C to quit)`);
+});
+// handle error
+server.on("error", (err) => {
+    if (err.syscall !== "listen") throw err;
+    switch (error.code) {
+        case "EACCES":
+            console.error(`${PORT} is requires elevated privileges`);
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(`${PORT} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+});
