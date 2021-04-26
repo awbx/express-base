476bc3aacf726b235c557da3cb0ccbfff4e4c6d0
Abdelhadi <abdo.sabani30@gmail.com>
2021-04-26 22:08:49 +0000

    Add logger & logger_format

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 "use strict";
 const express = require("express");
+const logger = require("morgan");
 
 // inlcude .env  variables
 require("dotenv").config("../.env");
 const env = process.env.ENV;
 const {
     name,
-    app: { port, debug },
+    app: { port, debug, logger_format },
 } = require("../config/settings")(env);
 
 // create instance from express
@@ -17,4 +18,6 @@ app.set("port", port);
 app.set("env", name);
 app.set("debugger", debug);
 
+// initialize middlewares
+if (debug) app.use(logger(logger_format));
 module.exports = app;
