77ada9b6726341255550f4ac4aa465c745d79835
Abdelhadi <abdo.sabani30@gmail.com>
2021-04-28 05:51:06 +0000

    Add helmet middleware to secure express app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const express = require("express");
 const logger = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
 
 // inlcude .env  variables
@@ -21,6 +22,7 @@ app.set("env", name);
 app.set("debugger", debug);
 
 // initialize middlewares
+app.use(helmet());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
