8dd7c7bb4b78e33d838fd2364ab7ca947706cfc9
Abdelhadi <abdo.sabani30@gmail.com>
2021-04-29 06:04:19 +0000

    Add test for home route

---
+++ test/routes/home.test.js
@@ -0,0 +1,10 @@
+"use strict";
+const request = require("supertest");
+const app = require("./../../app");
+describe("home route test", () => {
+    it("should test if body==='Express App' && status===200 ", async () => {
+        const res = await request(app).get("/home");
+        expect(res.status).toEqual(200);
+        expect(res.body).toEqual("Express App");
+    });
+});
