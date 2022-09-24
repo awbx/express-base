9032045e34f503d0af0313728fd98c7ea3e8fa8c
Abdelhadi Sabani <asabani@student.1337.ma>
2022-09-24 04:18:50 +0100

    imporve: change the logic of errorHandler middleware

--- app/middlewares/errorHandler.js
+++ app/middlewares/errorHandler.js
@@ -1,11 +1,27 @@
 "use strict";
+
+let trustedEnvs = ["development", "testing"];
+
 module.exports = (err, req, res, next) => {
-    const env = req.app.get("env");
-    const is_dev_or_test = env === "development" || env === "testing";
-    const error = {
-        message: err.message,
-    };
-    // error.stack = is_dev ? err.stack : null;
-    res.status(err.status || 500);
-    res.json(is_dev_or_test ? error : {});
+    try {
+        const env = req.app.get("config").name;
+
+        const isTrustedEnv = trustedEnvs.includes(env);
+
+        const error = {
+            message: err.message,
+        };
+
+        if (!isTrustedEnv)
+            error.message = "Something went wrong, please try again!";
+        else {
+            error.stack = err.stack;
+            console.debug(err);
+        }
+        
+        res.status(err.status || 500);
+        res.json(error);
+    } catch (err) {
+        next(err);
+    }
 };
